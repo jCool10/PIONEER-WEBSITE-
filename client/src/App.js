3d2b600526cf2f4646e07dfe50e07feaa5a8e249
jCool10 <valentinohoang1908@gmail.com>
2022-09-08 23:51:44 +0700

    set up tailwindcss, antd, axios

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 
 function App() {
-  return <div className="App">Pioneer website</div>;
+  return <div className="App text-red-900 bg-black">Pioneer website</div>;
 }
 
 export default App;
