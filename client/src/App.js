131650932f997077dfcb1064e9b69338848da2a9
jCool10 <valentinohoang1908@gmail.com>
2022-09-13 17:27:10 +0700

    Update routers

--- client/src/App.js
+++ client/src/App.js
@@ -1,52 +1,18 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
-import HomeTemplate from './templates/HomeTemplate';
-import { Home, Members, Product, Events, Contact } from './pages';
+import { Home, Members, Product, Events, Contact, AppLayout } from './pages';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route
-          path="/"
-          element={
-            <HomeTemplate>
-              <Home />
-            </HomeTemplate>
-          }
-        />
-        <Route
-          path="/members"
-          element={
-            <HomeTemplate>
-              <Members />
-            </HomeTemplate>
-          }
-        />
-        <Route
-          path="/product"
-          element={
-            <HomeTemplate>
-              <Product />
-            </HomeTemplate>
-          }
-        />
-        <Route
-          path="/events"
-          element={
-            <HomeTemplate>
-              <Events />
-            </HomeTemplate>
-          }
-        />
-        <Route
-          path="/contact"
-          element={
-            <HomeTemplate>
-              <Contact />
-            </HomeTemplate>
-          }
-        />
+        <Route path="/" element={<AppLayout />}>
+          <Route index element={<Home />} />
+          <Route path="/members" element={<Members />} />
+          <Route path="/product" element={<Product />} />
+          <Route path="/events" element={<Events />} />
+          <Route path="/contact" element={<Contact />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
