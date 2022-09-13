592900d416b77f88d644db234ec99f98b0aba27f
jCool10 <valentinohoang1908@gmail.com>
2022-09-13 17:30:23 +0700

    set up router

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,21 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import { Home, Members, Product, Events, Contact, AppLayout } from './pages';
 
 function App() {
-  return <div className="App text-red-900 bg-black">Pioneer website</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<AppLayout />}>
+          <Route index element={<Home />} />
+          <Route path="/members" element={<Members />} />
+          <Route path="/product" element={<Product />} />
+          <Route path="/events" element={<Events />} />
+          <Route path="/contact" element={<Contact />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
