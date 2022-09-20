42195af76c8b514d01d63ed61a2f4ad987c8ea03
jCool10 <valentinohoang1908@gmail.com>
2022-09-20 20:29:06 +0700

    Up load pages folder

---
+++ client/src/pages/index.js
@@ -0,0 +1,6 @@
+export { default as AppLayout } from './AppLayout';
+export { default as Home } from './Home/Home';
+export { default as Members } from './Members/Members';
+export { default as Product } from './Product/Product';
+export { default as Events } from './Events/Events';
+export { default as Contact } from './Contact/Contact';
