04573c2bd00693ce743e41f85bd478e2715815ac
Rekiichan <dtnhan.ete.dut@gmail.com>
2022-12-22 00:09:02 +0700

    fix module changename

--- client/public/FitNameFile.py
+++ client/public/FitNameFile.py
@@ -1,15 +1,23 @@
+import os
 from os import walk
 from os import listdir
 from os.path import isfile, join
 
-mypath = 'imgMembers/'
+mypath = "D:\WorkspaceCLB\pioneerWebsite\client\public\imgMembers\\"
+print(mypath)
 onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
 f = []
 for (dirpath, dirnames, filenames) in walk(mypath):
     f.extend(filenames)
     break
 
-for i in f:
-    i = i.lower()
-    i = i.replace(' ','-')
-    print(i)
+# print(onlyfiles)
+
+for i in onlyfiles:
+    oldname = mypath + i
+    newname = i.lower().replace(' ','-')
+    newname = mypath + newname
+    os.rename(oldname, newname)
+    print(oldname, newname)
+    
+print('finish')
